1bd645cf322e7b46691b8e502a823c67622b4f7e
Andrew McGuire <andrew.mcguire@daveramsey.com>
2019-09-14 17:05:53 -0500

    add args to minimizer_kwargs

--- convoys/regression.py
+++ convoys/regression.py
@@ -218,7 +218,8 @@ class GeneralizedGamma(RegressionModel):
 
         minimizer_kwargs = {
             'method': 'SLSQP', 'jac': jac,
-            'options':{'maxiter': 9999}
+            'options':{'maxiter': 9999},
+            'args': args
         }
 
         # Find the maximum a posteriori of the distribution
